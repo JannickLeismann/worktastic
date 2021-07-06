1fc2692f750723f4a8bd175e9a06744240ba70ed
JannickL <jannick.leismann@gmx.de>
2021-07-06 17:35:41 +0200

    Files hochladen und job postings speichern/übeschreiben

--- Worktastic/Worktastic/Controllers/JobPostingController.cs
+++ Worktastic/Worktastic/Controllers/JobPostingController.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Worktastic.Data;
 using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
     public class JobPostingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,9 +29,44 @@ namespace Worktastic.Controllers
             return View();
         }
 
-        public IActionResult CreateEditJob(JobPosting jobPosting)
+        public IActionResult CreateEditJob(JobPosting jobPosting, IFormFile file)
         {
-            // TODO: write jobposting to db
+            if(file != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    jobPosting.CompanyImage = bytes;
+                }
+            }
+
+            if(jobPosting.Id == 0)
+            {
+                // Add new job if not editing
+                _context.JobPostings.Add(jobPosting);
+            } else
+            {
+                var jobFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == jobPosting.Id);
+
+                if(jobFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                jobFromDb.CompanyImage = jobPosting.CompanyImage;
+                jobFromDb.CompanyName = jobPosting.CompanyName;
+                jobFromDb.ContactMail = jobFromDb.ContactMail;
+                jobFromDb.ContactPhone = jobFromDb.ContactPhone;
+                jobFromDb.ContactWebsite = jobFromDb.ContactWebsite;
+                jobFromDb.Description = jobFromDb.Description;
+                jobFromDb.JobLocation = jobFromDb.JobLocation;
+                jobFromDb.JobTitle = jobFromDb.JobTitle;
+                jobFromDb.Salary = jobFromDb.Salary;
+                jobFromDb.StartDate = jobFromDb.StartDate;
+            }
+
+            _context.SaveChanges();
 
             return RedirectToAction("Index");
         }
