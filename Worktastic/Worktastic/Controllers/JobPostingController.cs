74a79e6b0d14e198972cf8651e1ef07a06a37da7
JannickL <jannick.leismann@gmx.de>
2021-07-06 15:53:21 +0200

    Databinding des JobPostings

--- Worktastic/Worktastic/Controllers/JobPostingController.cs
+++ Worktastic/Worktastic/Controllers/JobPostingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
@@ -16,6 +17,13 @@ namespace Worktastic.Controllers
         public IActionResult CreateEditJobPosting(int id)
         {
             return View();
+        }
+
+        public IActionResult CreateEditJob(JobPosting jobPosting)
+        {
+            // TODO: write jobposting to db
+
+            return RedirectToAction("Index");
         }
     }
 }
