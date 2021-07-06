4174daa0eb6e1d3f80b9e3cf7ff104720a738b4f
JannickL <jannick.leismann@gmx.de>
2021-07-06 15:13:44 +0200

    Views und Routing vorbereiten

---
+++ Worktastic/Worktastic/Controllers/JobPostingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Worktastic.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditJobPosting(int id)
+        {
+            return View();
+        }
+    }
+}
