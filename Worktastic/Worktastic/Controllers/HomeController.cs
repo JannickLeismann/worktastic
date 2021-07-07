842a211063107ca9988321e8455150a57ece4341
JannickL <jannick.leismann@gmx.de>
2021-07-07 11:48:41 +0200

    Suchfilter einbauen mit Partial Views

--- Worktastic/Worktastic/Controllers/HomeController.cs
+++ Worktastic/Worktastic/Controllers/HomeController.cs
@@ -23,8 +23,7 @@ namespace Worktastic.Controllers
 
         public IActionResult Index()
         {
-            var allJobPostings = _context.JobPostings.ToList();
-            return View(allJobPostings);
+            return View();
         }
 
         public IActionResult Privacy()
@@ -50,6 +49,20 @@ namespace Worktastic.Controllers
                 return NotFound();
 
             return Ok(jobPostingFromDb);
+        }
+
+        public IActionResult GetJobPostingsPartial(string query)
+        {
+            List<JobPosting> jobPostings = new List<JobPosting>();
+
+            if (string.IsNullOrWhiteSpace(query))
+                jobPostings = _context.JobPostings.ToList();
+            else
+                jobPostings = _context.JobPostings
+                    .Where(x => x.JobTitle.ToLower().Contains(query.ToLower()))
+                    .ToList();
+
+            return PartialView("_JobPostingListPartial", jobPostings);
         }
     }
 }
