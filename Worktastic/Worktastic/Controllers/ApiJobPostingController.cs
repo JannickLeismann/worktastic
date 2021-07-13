8f7e41678ae64404becfb144ba36198dec463806
JannickL <jannick.leismann@gmx.de>
2021-07-13 10:36:28 +0200

    API Create Endpunkt

--- Worktastic/Worktastic/Controllers/ApiJobPostingController.cs
+++ Worktastic/Worktastic/Controllers/ApiJobPostingController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Worktastic.Data;
+using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
@@ -35,6 +36,18 @@ namespace Worktastic.Controllers
                 return NotFound();
 
             return Ok(jobPosting);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(JobPosting jobPosting)
+        {
+            if (jobPosting.Id != 0)
+                return BadRequest();
+
+            _context.JobPostings.Add(jobPosting);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
