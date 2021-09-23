4ec78d77ba7b83bdce7362139e1ba4b5cb9f7d77
JannickL <jannick.leismann@gmx.de>
2021-09-23 11:56:23 +0200

    Login page modified

---
+++ Worktastic/Worktastic/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Worktastic.Data;
+
+[assembly: HostingStartup(typeof(Worktastic.Areas.Identity.IdentityHostingStartup))]
+namespace Worktastic.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
